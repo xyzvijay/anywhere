eed4b2322fe35741908f55e36289a8f5d8712d69
xyzvijay <121185621+xyzvijay@users.noreply.github.com>
2022-12-21 22:49:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Components/NavbarComp';
+import FormsComp from './Components/FormsComp';
+
+function App() {
+  return (
+    <div>
+      <NavbarComp/>
+      <FormsComp/>
+    </div>
+  );
+}
+
+export default App;
